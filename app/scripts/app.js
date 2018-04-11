1b29126615aac1a99aaa9a196464e7d03c08647c
hstetts@gmail.com <hstetts@gmail.com>
2018-04-11 08:40:34 -0400

    Start timer code with timecode: incomplete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,7 @@
 
 
      angular
-         .module('bloctime', ['ui.router', 'firebase'])
+         .module('bloctime', ['ui.router', 'firebase', ])
+         .filter('timecode', ['timecode'])
          .config(config);
  })();
