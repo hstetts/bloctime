90230e49c5e9be5852db68e03a255dbedf93757c
hstetts@gmail.com <hstetts@gmail.com>
2018-04-16 07:55:45 -0400

    Add 5 min break

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,44 +2,71 @@
     function HomeCtrl($scope, $interval, $filter) {
 
 		//Initialize variables
-		$scope.count = 1500;
-    $scope.timerRunning = false;
+		$scope.count = 1500; //1500;
+    $scope.breakSession = 300;
     $scope.Timer = null;
+    $scope.onBreak = false;
+    $scope.onWork = true;
 
 
 
-		//Start button
 		$scope.start = function() {
-      $scope.timerRunning = true;
-       $scope.Timer = $interval(function() {
-        if($scope.count == 0) {
-  			     $interval.cancel();
+      $scope.onWork = true;
+      $scope.onBreak = false;
+      $scope.count = 1500;
+      startWork = $interval(function() {
+        if($scope.count <= 0) {
+  			     $interval.cancel(startWork);
+             $scope.onBreak = true;
              return;
   			}
 
   				$scope.count--;
   			}, 1000);
-  	  };
-
+  	  }
 
 
 		$scope.stop = function() {
-        $scope.timerRunning = false;
+      $scope.onWork = false;
         if (angular.isDefined($scope.Timer)) {
-              $interval.cancel($scope.Timer);
-              $scope.Timer = null;
-            }
+            $interval.cancel(startWork);
+            $scope.Timer = null;
+          }
 		};
 
-
 		$scope.reset = function() {
-      $scope.timerRunning = false;
-			$interval.cancel();
-			$scope.count = 1500;
+       $scope.onWork = false;
+			 $interval.cancel(startWork);
+			 $scope.count = 1500;
 		}
-  }
 
+    $scope.takeBreak = function() {
+       $scope.onBreak = true;
+       $scope.breakSession = 300;
+       startBreak = $interval(function() {
+         if($scope.breakSession <= 0) {
+  			     $interval.cancel(startBreak);
+             $scope.onBreak = false;
+             $scope.onWork = true;
+             return;
+    		 }
+         else if($scope.count == 0) {
+             $scope.onWork = false;
+             $scope.onBreak = true;
 
+      			 $scope.breakSession--;
+         }
+    	 }, 1000);
+     }
+  };
+
+    // $scope.resetWork = function() {
+    //   if (scope.count == 0 && scope.breakSession == 0) {
+    //       $scope.onWork = true;
+    //       $scope.start();
+    //       $scope.count = 1500;
+    //   }
+    // }
 
     angular
         .module('blocTime')
