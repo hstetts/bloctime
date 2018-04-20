7a7ff7ea5dabe3d874554dcc317456d24ff6f0b9
hstetts@gmail.com <hstetts@gmail.com>
2018-04-20 11:40:31 -0400

    Refactor short and long break; update buttons: complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,8 +2,10 @@
     function HomeCtrl($scope, $interval, $filter) {
 
 		//Initialize variables
-		$scope.count = 5; //1500;
-    $scope.breakSession = 4; //300//1800
+    var workSession = 1500;
+    var short_breakSession = 300;
+    var long_breakSession = 1800; //300//1800
+		$scope.count = workSession; //1500;
     $scope.Timer = null;
     $scope.onBreak = false;
     $scope.timerRunning = true;
@@ -12,44 +14,46 @@
 
 		$scope.start = function() {
       $scope.timerRunning = true;
-      $scope.count = 5;
+
 
       startWork = $interval(function() {
         $scope.count--;
 
-        if($scope.count = 0) {
+        if($scope.count === 0) {
 			     $interval.cancel(startWork);
            $scope.timerRunning = false;
-           //console.log("Work Session #: " + $scope.num_workSessions);
 
           if(!$scope.onBreak) {
             $scope.num_workSessions++;
             $scope.onBreak = true;
-            console.log("start work!")
+            $scope.count = short_breakSession;
 
-              if($scope.num_workSessions % 4 == 0) {
-                $scope.breakSession = 3; //1800
+
+              if($scope.num_workSessions % 4 === 0) {
+                console.log("Work Session #: " + $scope.num_workSessions);
+                console.log("long break")
+                $scope.count = long_breakSession;
                 $scope.timerRunning = false;
               } else {
-                $scope.count = 2;
+                $scope.count = short_breakSession;
+                console.log("short break")
                 $scope.timerRunning = false;
               }
+
+          } else {
+            $scope.onBreak = false;
+            $scope.count = workSession;
           }
-
-        $scope.onBreak = false;
-        $scope.count = 5;
-
-      }
-    }, 1000);
+       }
+     }, 1000);
 
 
 		$scope.reset = function() {
 			 $interval.cancel(startWork);
-			 $scope.count = 5;
+			 $scope.count = workSession;
        $scope.timerRunning = false;
 		}
   }
-
 };
 
 // $scope.stop = function() {
@@ -60,49 +64,8 @@
 //       }
 // };
 
-   //  $scope.take_break = function() {
-   //     $scope.onBreak = true;
-   //     startBreak = $interval(function() {
-   //       if($scope.breakSession <= 0) {
-  	// 		     $interval.cancel(startBreak);
-   //           console.log("cancel break start");
-   //           return;
-   //         }
-   //  		 else if($scope.num_workSessions % 4 == 0) {
-   //         console.log("long break!")
-   //         $scope.breakSession = 4;
-   //         $scope.breakSession--;
-   //       }
-   //
-   //         $scope.breakSession--;
-   //         console.log("short break!")
-   //  	 }, 1000);
-   //   }
-   // };
 
-    // $scope.take_longBreak = function() {
-    //     $scope.onBreak = true;
-    //     $scope.breakSession = 3;
-    //     start_longBreak = $interval(function() {
-    //        if($scope.take_longBreak <= 0)
-    //          $interval.cancel(start_longBreak);
-    //        else if ($scope.num_workSessions % 4 == 0) {
-    //          console.log("long break!")
-    //          $scope.onWork = false;
-    //          $scope.onBreak = true;
-    //          $scope.breakSession--;
-    //        }
-    //       }, 1000);
-    //   }
-    //};
 
-    // $scope.resetWork = function() {
-    //   if (scope.count == 0 && scope.breakSession == 0) {
-    //       $scope.onWork = true;
-    //       $scope.start();
-    //       $scope.count = 1500;
-    //   }
-    // }
 
     angular
         .module('blocTime')
